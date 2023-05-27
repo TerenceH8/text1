e740ed0fa1a49f8f194db0e6e39eaf8f07785361
Terence <752102166@163.com>
2023-05-27 21:30:53 +0800

    第2次提交测试

---
+++ 11_if_多分支.py
@@ -0,0 +1,13 @@
+money = int(input("请输入兜里的钱："))
+
+if money > 8888:
+    print("买个8848钛合金小手机")
+
+elif money > 6666:
+    print("买个小米也不错")
+
+elif money > 1000:
+    print("买个998也不伤")
+
+else:
+    print("买个05年的诺基亚也不错")
